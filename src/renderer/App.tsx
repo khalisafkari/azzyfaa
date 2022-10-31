e6fa63200c837fb5b5d05b755cc58fb678fe93b3
khalis afkari <khalis411@gmail.com>
2022-10-31 16:59:42 +0700

    first commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,75 @@
+import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+const Page = () => {
+  return (
+    <div>
+         <Link to={'/'}>
+            <button type='button'>
+              <span role="img" aria-label="folded hands">
+                🙏
+              </span>
+              page
+            </button>
+        </Link>
+    </div>
+  )
+}
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <Link to={'/page'}>
+            <button type='button'>
+              <span role="img" aria-label="folded hands">
+                🙏
+              </span>
+              page
+            </button>
+        </Link>
+        <a
+          
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="folded hands">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+        <Route path='/page' element={<Page />} />
+      </Routes>
+    </Router>
+  );
+}
